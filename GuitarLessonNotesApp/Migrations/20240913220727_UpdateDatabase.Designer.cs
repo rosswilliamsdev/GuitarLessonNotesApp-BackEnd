029c6af1c0b0306b608888c3f30bc038f701a9f5
rosswilliamsdev <rwillguitar@gmail.com>
2024-09-13 17:17:17 -0500

    Admin Login Successful- salt error fixed

--- GuitarLessonNotesApp/Migrations/20240913220727_UpdateDatabase.Designer.cs
+++ GuitarLessonNotesApp/Migrations/20240913220727_UpdateDatabase.Designer.cs
@@ -4,6 +4,7 @@ using GuitarLessonNotesApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GuitarLessonNotesApp.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240913220727_UpdateDatabase")]
+    partial class UpdateDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ GuitarLessonNotesApp/Migrations/20240913220727_UpdateDatabase.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GuitarLessonNotesApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "PasswordHash",
+                value: "$2a$12$DgpT/ER.YxCbTTwGoh5DnuDPoEWhj2H4XIY1SR7Yii7CL6PpK8z8S");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "PasswordHash",
+                value: "$2a$12$dBDgbAMoz0myWvpyftZEduvMjYObsO3AZgONkAT4pxyF38cOII8Ai");
+        }
+    }
+}
